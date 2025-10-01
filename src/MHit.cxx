ec4d729cb7b8217c2a61192e9904e548f5e2ea1c
Julian Gerber <jmgerber@ucsd.edu>
2025-09-30 21:30:16 -0700

    Previously only reading out X and Y energy res for DAT files version 2, now doing it for version 1 as well

--- src/MHit.cxx
+++ src/MHit.cxx
@@ -135,7 +135,7 @@ bool MHit::StreamDat(ostream& S, int Version)
   //! Stream the content to an ASCII file 
   
   if( Version == 1 ){
-     S<<"HT "<<m_Position.GetX()<<" "<<m_Position.GetY()<<" "<<m_Position.GetZ()<<" "<<m_Energy<<" "<<m_XEnergy<<" "<<m_YEnergy<<endl;
+      S<<"HT "<<m_Position.GetX()<<" "<<m_Position.GetY()<<" "<<m_Position.GetZ()<<" "<<m_Energy<<" "<<m_XEnergy<<" "<<m_YEnergy<<" "<<m_XEnergyRes<<" "<<m_YEnergyRes<<endl;
   } else if( Version == 2 ){
 	  //stream the hit information, then stream the strip hit info for this hit so that 
 	  //we will know which strip hits were associated with which hits
